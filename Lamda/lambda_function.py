a28d7117d6f7084cb322168aa0350c6f397e6d1f
Mohammed-Feroz2245 <feroz13143@gmail.com>
2026-01-21 11:31:21 +0100

    Initial CI-ready ML project

--- Lamda/lambda_function.py
+++ Lamda/lambda_function.py
@@ -1,10 +1,9 @@
-#from src.script import train_and_upload
+from src.training.script import train_and_upload
 
 def lambda_handler(event, context):
-    #print("Event received:", event)
-    #ccuracy = train_and_upload()
-
+    acc = train_and_upload()
     return {
         "statusCode": 200,
         "message": "Model trained and uploaded to S3"
+        "accuracy": acc
     }
