697d592d5bbc06587e377f843ea620c45c121a6d
Mohammed-Feroz2245 <feroz13143@gmail.com>
2026-01-21 12:21:18 +0100

    Check

--- src/training/script.py
+++ src/training/script.py
@@ -9,20 +9,17 @@ BUCKET_NAME = "course-completion-ml-artifacts"
 DATA_KEY = "data/online_course_completion(3).csv"
 MODEL_KEY = "artifacts/model.pkl"
 
-s3 = boto3.client("s3")
-
 def train_and_upload():
-    # Lambda writable paths
+    s3 = boto3.client("s3")
+
     data_path = "/tmp/data.csv"
     model_path = "/tmp/model.pkl"
 
-    # STEP 1: DOWNLOAD DATA
     s3.download_file(BUCKET_NAME, DATA_KEY, data_path)
     df = pd.read_csv(data_path)
 
-    # STEP 2: CLEANING
     df.drop_duplicates(inplace=True)
-
+    
     df.drop(
         [
             'height_cm', 'weight_kg', 'num_siblings', 'has_pet',
@@ -32,28 +29,14 @@ def train_and_upload():
         inplace=True,
         errors="ignore"
     )
-
     df.dropna(inplace=True)
 
-    # STEP 3: ENCODING
-    dummies = pd.get_dummies(df["preferred_device"], drop_first=True).astype(int)
+    dummies = pd.get_dummies(df["preferred_device"], drop_first=True)
     df = pd.concat([df, dummies], axis=1)
 
-    X = df.drop(
-        [
-            "continent",
-            "country",
-            "education_level",
-            "preferred_device",
-            "completed_course"
-        ],
-        axis=1,
-        errors="ignore"
-    )
-
+    X = df.drop(["completed_course", "preferred_device"], axis=1)
     y = df["completed_course"]
 
-    # STEP 4: TRAIN
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
@@ -61,16 +44,11 @@ def train_and_upload():
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
+    acc = accuracy_score(y_test, model.predict(X_test))
     print("Accuracy:", acc)
 
-    # STEP 5: SAVE MODEL
     with open(model_path, "wb") as f:
         pickle.dump(model, f)
 
-    # STEP 6: UPLOAD MODEL
     s3.upload_file(model_path, BUCKET_NAME, MODEL_KEY)
-
     return acc
